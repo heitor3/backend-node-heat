4ea28cca0f0ae5170eee0cb60f9a8362914e0723
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-10-20 18:11:26 -0300

    create authenticate user

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
-import axios from "axios";
-
+import axios from 'axios';
+import prismaClient from '../prisma';
+import { sign } from 'jsonwebtoken';
 
 interface IAccessTokenResponse {
   access_token: string
@@ -37,9 +38,58 @@ class AuthenticateUserService {
       headers: {
         authorization: `Bearer ${accessTokenResponse.access_token}`
       }
-    })
+    });
 
-    return response.data;
+    const {
+      id,
+      login,
+      avatar_url,
+      name,
+      followers,
+      following,
+      html_url,
+      location,
+      public_repos
+    } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        git_id: id
+      }
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          git_id: id,
+          login,
+          avatar_url,
+          name,
+          followers,
+          following,
+          html_url,
+          location,
+          public_repos,
+        }
+      });
+    };
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id
+        }
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d"
+      }
+    )
+
+    return { token, user };
   }
 }
 
